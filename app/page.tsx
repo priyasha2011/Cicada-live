e671022718b4a5b0ab9cceefa9cd59b3201d6caa
priyasha2011 <priyashamohanty03@gmail.com>
2024-09-20 02:43:53 +0530

    finally works

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,24 @@
 'use client'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 import { useEffect } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
 import Sum from './sum/page';
-import Footer from "./components/footer"; // Import the Footer component
+import Footer from "./components/footer";
 
 export default function App() {
+  const router = useRouter();
+  const pathname = usePathname();
+
   useEffect(() => {
-    const handleContextMenu = (event: MouseEvent) => {
-      event.preventDefault();
-    };
-
-    const handleCopy = (event: ClipboardEvent) => {
-      event.preventDefault();
-    };
-
-    const handlePaste = (event: ClipboardEvent) => {
-      event.preventDefault();
-    };
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (
-        event.ctrlKey &&
-        (event.key === "c" || event.key === "v" || event.key === "x")
-      ) {
-        event.preventDefault();
-      }
-      if (event.ctrlKey && event.shiftKey && event.key === "I") {
-        event.preventDefault();
-      }
-    };
-
-    document.addEventListener("contextmenu", handleContextMenu);
-    document.addEventListener("copy", handleCopy);
-    document.addEventListener("paste", handlePaste);
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("contextmenu", handleContextMenu);
-      document.removeEventListener("copy", handleCopy);
-      document.removeEventListener("paste", handlePaste);
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+    if (pathname === '/') {
+      router.push('/sum');
+    }
+  }, [pathname, router]);
 
   return (
-    <Router>
-      <div style={{ minHeight: '100vh', position: 'relative', paddingBottom: '60px' }}>
-        <Routes>
-          <Route path="/sum" element={<Sum />} />
-          <Route path="/" element={<Navigate replace to="/sum" />} />
-        </Routes>
-        <Footer /> {/* Add the Footer component */}
-      </div>
-    </Router>
+    <div className="min-h-screen relative pb-16">
+      {pathname === '/sum' && <Sum />}
+      <Footer />
+    </div>
   );
 }
